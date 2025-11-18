e43fdc12b616448e558c66d47100578dd4c69194
thiagodeas <sousaalvesth@gmail.com>
2025-11-18 11:59:54 -0300

    first commit

---
+++ src/main/java/com/thiagoalves/estaparbackendtest/EstaparbackendtestApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoalves.estaparbackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstaparbackendtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstaparbackendtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagoalves/estaparbackendtest/EstaparbackendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoalves.estaparbackendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstaparbackendtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
