e5195cf39daecfc3b2a0bceea40e53ab2ef53740
thiagodeas <sousaalvesth@gmail.com>
2025-11-19 17:11:09 -0300

    feat(dto): add spot dto

---
+++ src/main/java/com/thiagoalves/estaparbackendtest/dtos/SpotDTO.java
@@ -0,0 +1,10 @@
+package com.thiagoalves.estaparbackendtest.dtos;
+
+public class SpotDTO {
+
+    public Long id;
+    public String sector;
+    public Double lat;
+    public Double lng;
+    public Boolean occupied;
+}
