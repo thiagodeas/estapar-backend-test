89fac223b9352fd3dcff480b4f8a8b9733ba574f
thiagodeas <sousaalvesth@gmail.com>
2025-11-19 17:11:51 -0300

    feat(dto): add generic webhook dto

---
+++ src/main/java/com/thiagoalves/estaparbackendtest/dtos/webhook/GenericWebhookDTO.java
@@ -0,0 +1,11 @@
+package com.thiagoalves.estaparbackendtest.dtos.webhook;
+
+public class GenericWebhookDTO {
+    public String license_plate;
+    public String event_type;
+    public String entry_time;
+    public String exit_time;
+    public Double lat;
+    public Double lng;
+}
+
