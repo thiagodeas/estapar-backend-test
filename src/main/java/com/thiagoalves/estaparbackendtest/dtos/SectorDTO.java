847a8e5921478296200cbf1c1079c794baa1fd59
thiagodeas <sousaalvesth@gmail.com>
2025-11-19 17:10:59 -0300

    feat(dto): add sector dto

---
+++ src/main/java/com/thiagoalves/estaparbackendtest/dtos/SectorDTO.java
@@ -0,0 +1,13 @@
+package com.thiagoalves.estaparbackendtest.dtos;
+
+import java.math.BigDecimal;
+
+public class SectorDTO {
+
+    public String sector;
+    public BigDecimal base_price;
+    public Integer max_capacity;
+    public String open_hour;
+    public String close_hour;
+    public Integer duration_limit_minutes;
+}
