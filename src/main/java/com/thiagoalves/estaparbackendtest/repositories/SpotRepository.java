1628513837fa045bcacbab988ca370f6e4241de4
thiagodeas <sousaalvesth@gmail.com>
2025-11-22 17:30:50 -0300

    feat(repository): add methods to spot repository

--- src/main/java/com/thiagoalves/estaparbackendtest/repositories/SpotRepository.java
+++ src/main/java/com/thiagoalves/estaparbackendtest/repositories/SpotRepository.java
@@ -3,9 +3,11 @@ package com.thiagoalves.estaparbackendtest.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.thiagoalves.estaparbackendtest.models.Sector;
 import com.thiagoalves.estaparbackendtest.models.Spot;
 
 @Repository
 public interface SpotRepository extends JpaRepository<Spot, Long> {
-    
+    int countBySectorAndOccupiedTrue(Sector sector);
+    Spot findByLatAndLng(Double lat, Double lng);
 }
