e9b34bf09ead7ec6e812c57be1e5a51532bbf5de
thiagodeas <sousaalvesth@gmail.com>
2025-11-22 17:29:30 -0300

    refactor(dto): simplify garage response dto

--- src/main/java/com/thiagoalves/estaparbackendtest/dtos/GarageResponseDTO.java
+++ src/main/java/com/thiagoalves/estaparbackendtest/dtos/GarageResponseDTO.java
@@ -4,22 +4,6 @@ import java.util.List;
 
 public class GarageResponseDTO {
 
-    private List<SectorDTO> garage;
-    private List<SpotDTO> spots;
-
-    public List<SectorDTO> getGarage() {
-        return garage;
-    }
-
-    public void setGarage(List<SectorDTO> garage) {
-        this.garage = garage;
-    }
-
-    public List<SpotDTO> getSpots() {
-        return spots;
-    }
-
-    public void setSpots(List<SpotDTO> spots) {
-        this.spots = spots;
-    }
+    public List<SectorDTO> garage;
+    public List<SpotDTO> spots;
 }
