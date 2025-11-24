b30dcde7a8da636acf46c95bde7b75d47cd17dbf
thiagodeas <sousaalvesth@gmail.com>
2025-11-23 21:25:29 -0300

    refactor(repository): add missing repository annotation

--- src/main/java/com/thiagoalves/estaparbackendtest/repositories/ParkingEventRepository.java
+++ src/main/java/com/thiagoalves/estaparbackendtest/repositories/ParkingEventRepository.java
@@ -3,10 +3,12 @@ package com.thiagoalves.estaparbackendtest.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.thiagoalves.estaparbackendtest.models.ParkingEvent;
 import com.thiagoalves.estaparbackendtest.models.Sector;
 
+@Repository
 public interface ParkingEventRepository extends JpaRepository<ParkingEvent, Long>{
     
     ParkingEvent findByLicensePlateAndExitTimeIsNull(String licensePlate);
@@ -16,5 +18,4 @@ public interface ParkingEventRepository extends JpaRepository<ParkingEvent, Long
     List<ParkingEvent> findBySector(Sector sector);
 
     int countBySectorAndExitTimeIsNull(Sector sector);
-
 }
